38b992f13d3f06aa283cc26ad6bef7a20bd8d78b
Pegasus Epsilon <pegasus@pimpninjas.org>
2013-08-29 17:07:27 -0400

    fixed an oops with pngify arguments

--- hugemandelbrot.sh
+++ hugemandelbrot.sh
@@ -11,6 +11,6 @@ mkfifo mandelbrot.map mandelbrot.msaa mandelbrot.rgb
 mandelbrot --crosstrap 0 .005 1 $((1920*4*16)) $((1080*4*16)) 0 0 2 1.25 -0.7766729 0.13661091 0 mandelbrot.map &
 render mandelbrot.map palette.bin 0 .015 mandelbrot.msaa &
 resample mandelbrot.msaa $((1920*4)) 16 mandelbrot.rgb &
-pngify mandelbrot.rgb $((1920*4)) mandelbrot.png
+pngify mandelbrot.rgb $((1920*4)) $((1080*4)) mandelbrot.png
 
 rm mandelbrot.map mandelbrot.msaa mandelbrot.rgb
