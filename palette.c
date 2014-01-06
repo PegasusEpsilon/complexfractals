76fbfc4f2ccbea9008d689335d1de2699e515628
Pegasus Epsilon <pegasus@pimpninjas.org>
2014-01-06 03:36:04 -0500

    Fixed a spurious warning, tweaked some variable names

--- palette.c
+++ palette.c
@@ -49,7 +49,7 @@ struct POINT {
 };
 
 struct CHANNEL {
-	struct POINT *p;
+	struct POINT *points;
 	size_t length;
 };
 
@@ -79,7 +79,7 @@ void die (const char *fmt, ...) {
  * like linear interpolation (ugly, even modified), splines (complicated)
  * or curve fitting (in some cases, horrendously imprecise).
  */
-struct GRADIENT *generate_palette (const struct CHANNEL *points, struct GRADIENT *gradient) {
+struct GRADIENT *generate_palette (const struct CHANNEL *channels, struct GRADIENT *gradient) {
 	uint8_t c;	/* only three channels, after all */
 	size_t i;	/* current point in overall interpolation */
 	size_t a;	/* starting point for this segment of interpolation */
@@ -87,9 +87,9 @@ struct GRADIENT *generate_palette (const struct CHANNEL *points, struct GRADIENT
 
 	for (c = 0; c < CHANNELS; c++) {
 		/* point A = cyclically previous point */
-		a = points[c].p[0].x > 0 ? points[c].length - 1 : 0;
+		a = channels[c].points[0].x > 0 ? channels[c].length - 1 : 0;
 		/* point B = cyclically next point */
-		b = (a + 1) % points[c].length;
+		b = (a + 1) % channels[c].length;
 		/* for each point "i" in the palette... */
 		for (i = 0; i < gradient->length; i++) {
 			/* y = vertical coordinate ("magnitude") of the point */
@@ -97,26 +97,26 @@ struct GRADIENT *generate_palette (const struct CHANNEL *points, struct GRADIENT
 			/* x = horizontal coordinate ("timestamp") of the point */
 			double x = (double)i / (double)gradient->length;
 			/* dx = distance between A and B on the X axis (delta t of [a, b]) */
-			double dx = fmod(1 + points[c].p[b].x - points[c].p[a].x, 1.0);
+			double dx = fmod(1 + channels[c].points[b].x - channels[c].points[a].x, 1.0);
 
-			/* Move forward through the given points until we
-			 * find one we haven't surpassed yet. Any points
+			/* Move forward through the given channels until we
+			 * find one we haven't surpassed yet. Any channels
 			 * we skip entirely are lost in the noise of the
 			 * waveform. Such is the nature of the beast.
 			 */
-			while (x > points[c].p[b].x) {
-				b = ((a = (a + 1) % points[c].length) + 1) % points[c].length;
-				if (points[c].p[b].x < points[c].p[a].x) points[c].p[b].x++;
+			while (x > channels[c].points[b].x) {
+				b = ((a = (a + 1) % channels[c].length) + 1) % channels[c].length;
+				if (channels[c].points[b].x < channels[c].points[a].x) channels[c].points[b].x++;
 			}
 
-			x -= points[c].p[a].x;	/* shift X */
+			x -= channels[c].points[a].x;	/* shift X */
 			if (dx) /* no divide by zero please */
 				x /= dx;	/* scale X */
-			else
+			else if (channels[c].points[a].x == channels[c].points[b].x)
 				puts("Warning: Two points specified on the same channel at the same time.");
 			y = (cos(M_PI * x) + 1) / 2;	/* interpolate! */
-			y *= points[c].p[a].y - points[c].p[b].y;	/* scale Y */
-			y += points[c].p[b].y;	/* shift Y */
+			y *= channels[c].points[a].y - channels[c].points[b].y;	/* scale Y */
+			y += channels[c].points[b].y;	/* shift Y */
 
 			gradient->x[i].y[c] = (uint8_t)(y * 255 + .5);
 		} /* for i < gradient->length */
@@ -134,41 +134,41 @@ void usage (const char *myself) {
 /* allocate storage for, and store, specified point */
 struct CHANNEL *add_point (struct CHANNEL *in, double x, double y) {
 	struct POINT *new;
-	new = realloc(in->p, (in->length + 1) * sizeof(*(in->p)));
+	new = realloc(in->points, (in->length + 1) * sizeof(*(in->points)));
 	if (!new)
 		fail("failed to allocate memory");
-	in->p = new;
-	in->p[in->length].x = x;
-	in->p[in->length].y = y;
+	in->points = new;
+	in->points[in->length].x = x;
+	in->points[in->length].y = y;
 	in->length++;
 	return in;
 }
 
-/* allocate storage for, and store, points as
+/* allocate storage for, and store, channels as
  * specified, for each channel, all at once
  */
-void add_syncpoint (struct CHANNEL *points, double x, char *line, off_t *off) {
+void add_syncpoint (struct CHANNEL *channels, double x, char *line, off_t *off) {
 	uint8_t c;
 	double y;
 	for (c = 0; c < CHANNELS; c++) {
 		line += *off;
 		sscanf(line, "%lf%jn", &y, off);
 		debug(",%f", y);
-		add_point(&(points[c]), x, y);
+		add_point(&(channels[c]), x, y);
 	}
 }
 
 /* print the parsed palette directives */
-void printparsed (struct CHANNEL *points) {
+void printparsed (struct CHANNEL *channels) {
 	uint8_t c;
 	size_t i;
 	for (c = 0; c < CHANNELS; c++)
-		for (i = 0; i < points[c].length; i++)
+		for (i = 0; i < channels[c].length; i++)
 			printf(
 					"%s: %f/%f\n",
 					channel[c],
-					points[c].p[i].x,
-					points[c].p[i].y
+					channels[c].points[i].x,
+					channels[c].points[i].y
 			);
 }
 
@@ -188,7 +188,7 @@ void printoutput (struct GRADIENT *gradient) {
 int main (int argc, char **argv) {
 	uint8_t c;
 	size_t i;
-	struct CHANNEL points[CHANNELS];
+	struct CHANNEL channels[CHANNELS];
 	struct GRADIENT gradient;
 	FILE *infile;
 	FILE *outfile;
@@ -207,9 +207,9 @@ int main (int argc, char **argv) {
 	/* init all storage */
 	gradient.length = 0;
 	for (c = 0; c < CHANNELS; c++)
-		points[c].length = 0;
+		channels[c].length = 0;
 	for (c = 0; c < CHANNELS; c++)
-		points[c].p = NULL;
+		channels[c].points = NULL;
 
 	/* read and parse the input file */
 	if (NULL == (infile = fopen(argv[1], "r")))
@@ -255,17 +255,17 @@ int main (int argc, char **argv) {
 		/* process actual control point directives */
 		if (sscanf(line+i, "RED%lf%lf", &x, &y)) {
 			debug("read RED control point %lf/%lf\n", x, y);
-			add_point(&points[RED], x, y);
+			add_point(&channels[RED], x, y);
 		} else if (sscanf(line+i, "GRN%lf%lf", &x, &y)) {
 			debug("read GRN control point %lf/%lf\n", x, y);
-			add_point(&points[GRN], x, y);
+			add_point(&channels[GRN], x, y);
 		} else if (sscanf(line+i, "BLU%lf%lf", &x, &y)) {
 			debug("read BLU control point %lf/%lf\n", x, y);
-			add_point(&points[BLU], x, y);
+			add_point(&channels[BLU], x, y);
 		} else if (sscanf(line+i, "RGB%lf%jn", &x, &off)) {
 			/* Tested and fixed. Not sure it's the best way, though. */
 			debug("read RGB control point %lf", x);
-			add_syncpoint(points, x, line+i, &off);
+			add_syncpoint(channels, x, line+i, &off);
 			if (&printf == debug) putchar('\n');
 		} else
 			printf("Warning: Unknown statement in config file: %s\n", line+i);
@@ -274,14 +274,14 @@ int main (int argc, char **argv) {
 
 	/* debug? print the parsed directives */
 	if (&printf == debug)
-		printparsed(points);
+		printparsed(channels);
 	/* make sure we got a gradient length */
 	if (!gradient.length) die("Config must specify a gradient length (LEN)");
 	/* this could be allocated above */
 	gradient.x = calloc(gradient.length, sizeof(*gradient.x));
 
 	/* actually generate the palette */
-	generate_palette(points, &gradient);
+	generate_palette(channels, &gradient);
 	/* debug? print the final palette data */
 	if (&printf == debug)
 		printoutput(&gradient);
@@ -294,7 +294,7 @@ int main (int argc, char **argv) {
 	/* clean up */
 	free(gradient.x);
 	for (c = 0; c < CHANNELS; c++)
-		free(points[c].p);
+		free(channels[c].points);
 
 	return 0;
 }
