10a603d2b74a42ccd72d1c6b58ac6079de586fbc
Pegasus Epsilon <pegasus@pimpninjas.org>
2014-05-22 07:33:28 +0200

    added a thetaless version of pixel2vector, plus a wrapper function to choose which to use for the rest of the execution

--- mapper.c
+++ mapper.c
@@ -16,7 +16,19 @@ COMPLEX pixelsize (const PIXEL *const img, const REGION *const window) {
 	return out;
 }
 
-COMPLEX pixel2vector (
+COMPLEX pixel2vector_NOTHETA (
+	const PIXEL *const in, const COMPLEX *const size,
+	const REGION *const window, const double theta
+) {
+	COMPLEX out;
+
+	out.R = window->center.R + (in->X * size->R - window->radius.R);
+	out.I = -window->center.I + (in->Y * size->I - window->radius.I);
+
+	return out;
+}
+
+COMPLEX pixel2vector_THETA (
 	const PIXEL *const in, const COMPLEX *const size,
 	const REGION *const window, const double theta
 ) {
@@ -30,4 +42,20 @@ COMPLEX pixel2vector (
 		+ (in->Y * size->I - window->radius.I) * cos(theta);
 
 	return out;
+}
+
+COMPLEX pixel2vector_START (
+	const PIXEL *, const COMPLEX *, const REGION *, double
+);
+COMPLEX (*pixel2vector)(
+	const PIXEL *const in, const COMPLEX *const size,
+	const REGION *const window, const double theta
+) = &pixel2vector_START;
+
+COMPLEX pixel2vector_START (
+	const PIXEL *const in, const COMPLEX *const size,
+	const REGION *const window, const double theta
+) {
+	pixel2vector = theta ? &pixel2vector_THETA : &pixel2vector_NOTHETA;
+	return pixel2vector(in, size, window, theta);
 }
