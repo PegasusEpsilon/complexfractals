70478a6d53ae6fec334091ee458f33d89e0e1a3e
Pegasus Epsilon <pegasus@pimpninjas.org>
2014-11-13 22:39:25 -0500

    Rewrote the tiler some. Probably still not done.

--- tiler.c
+++ tiler.c
@@ -1,3 +1,6 @@
+#define _POSIX_SOURCE	/* SSIZE_MAX */
+#include <limits.h> 	/* SSIZE_MAX */
+
 #include <stdio.h>  	/* printf(), puts(), sprintf(), fflush() */
 #include <stdlib.h> 	/* exit(), strtoul(), malloc() */
 
@@ -9,14 +12,14 @@
 
 #include <string.h> 	/* strlen() */
 
-#include <errno.h>  	/* EIO */
+#include <errno.h>  	/* EIO, errno */
 
 #ifdef _WIN32
 #	define PRIuSIZET "Iu"
-#	define PRIuOFFT "Id"
+#	define PRIdSSIZET "Id"
 #else
 #	define PRIuSIZET "zu"
-#	define PRIuOFFT "zd"
+#	define PRIdSSIZET "zd"
 #endif
 
 __attribute__((noreturn))
@@ -35,11 +38,24 @@ void usage (char *argv0) {
 __attribute__((noreturn))
 void fail (const char *msg) {   /* report function failures */
 	perror(msg);
-	exit(1);
+	exit(errno);
 }
 
 __attribute__((noreturn))
 void die (char *msg, int err) { puts(msg); exit(-err); }
+
+ssize_t fullread (int fd, void *buf, size_t count) {
+	if (count > SSIZE_MAX) die("row length too large, sorry", EIO);
+	ssize_t got = 0, total = 0;
+	for (;
+		total != count
+		&& 0 <= (got = read(fd, (char *)buf + total, count - (size_t)total));
+		total += got
+	) if (0 == got && EAGAIN != errno) { return total; }
+	if (0 > got) fail("read error");
+	if (total != count) fail("short read");
+	return total;
+}
 
 int main (int argc, char **argv) {
 	size_t width, height, columns, rows, rowlen, eof;
@@ -59,15 +75,14 @@ int main (int argc, char **argv) {
 		+ strlen("/row000col000.png\n"));
 
 	rowlen = width * columns;
+	if (SSIZE_MAX < rowlen) die("row length too large, sorry", EIO);
 	buf = malloc(rowlen);
 
-	if (rowlen != read(inputfd, buf, rowlen))
-		die("read error when starting", EIO);
-
+	fullread(inputfd, buf, rowlen);
 	eof = rows = 0;
 	do {
 		for (int c = 0; c < columns; c++) {
-			sprintf(outputfile, "%s/row%03" PRIuSIZET "col%03d.rgb",
+			sprintf(outputfile, "%s/row%02" PRIuSIZET "col%02d.rgb",
 				argv[5], rows, c);
 			outputfd[c] = open(outputfile, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 		}
@@ -76,7 +91,7 @@ int main (int argc, char **argv) {
 			for (size_t c = 0; c < columns; c++)
 				if (width != write(outputfd[c], buf + c * width, width))
 					die("write error", EIO);
-			eof = (rowlen != read(inputfd, buf, rowlen));
+			eof = (rowlen != fullread(inputfd, buf, rowlen));
 		}
 
 		for (int c = 0; c < columns; c++) close(outputfd[c]);
